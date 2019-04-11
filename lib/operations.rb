2721ffefba9e797494cdb32f12671aa445db75e7
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-11 21:41:07 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  elsif speed 
+  return false
+else true
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  if speed > 60
+    return true
+  end
+  speed < 40 ? true : false
 end
-	
 
 

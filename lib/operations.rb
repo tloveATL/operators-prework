38c73a0f4edc411defef1d01ee03d51790e2aac9
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-11 22:09:34 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,15 @@
 def unsafe?(speed)
   if speed > 60
     return true
+<<<<<<< HEAD
   elsif speed < 40
     return true
   elsif speed 
+=======
+  elsif speed < 60
+    return true
+  elsif speed < 40 || speed > 60
+>>>>>>> 97133cfb73b683f3d4afc0f05a6ef0d12cf56258
   return false
 else true
   end
